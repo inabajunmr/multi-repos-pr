bc4000d0471c17e16f9ad38d9662845dfa8f74e9
inabajunmr <inabajun.for.regi@gmail.com>
2019-01-11 21:17:06 +0900

    initial

---
+++ commits.sh
@@ -0,0 +1,17 @@
+BRANCH_NAME=BANCHNAME
+COMMIT_COMMENT="COMMITCOMMENT"
+PR_COMMENT="PRTITLE\n\nPRCOMMENT"
+while read line
+do
+  git clone git@github.com:${line}.git
+  cd "$(echo ${line} | sed -E s/^[^\/]+.//)"
+  git branch ${BRANCH_NAME}
+  git checkout ${BRANCH_NAME}
+  rsync -r ../source/ ./
+  git add .
+  git commit -m "${COMMIT_COMMENT}"
+  git push -u --set-upstream origin ${BRANCH_NAME}
+  echo ${PR_COMMENT} | hub pull-request -F -
+  cd -
+done < repos
+
